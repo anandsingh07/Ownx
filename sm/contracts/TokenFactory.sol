129aba232a07b4b67db39523bce8d2bb345a3815
Anand Singh <anandsinghas9777@gmail.com>
2025-12-28 00:13:18 +0530

    Add tokens status and information structure code

--- sm/contracts/TokenFactory.sol
+++ sm/contracts/TokenFactory.sol
@@ -1,9 +1,34 @@
-// SPDX-License-Identifer :MIT 
-pragma solidity ^0.8.20;
+// SPDX-License-Identifer:MIT 
+pragma solidity ^0.8.28;
 
-import './CreatorToken.sol';
+import "./CreatorToken.sol";
 
 contract TokenFactory {
 
-    
+    enum TokenStatus{
+
+        Active ,
+        Pending ,
+        Deactivated 
+
+    }
+
+    struct Tokeninfo {
+
+        address token ;
+        address creator ;
+        string name ;
+        string symbol ;
+        string description ;
+        string image ;
+        uint8 category ;
+        TokenStatus status ;
+        uint256 createdAt ;
+
+    }
+
+    Tokeninfo [] public alltokens ;
+
+    mapping(address => address[])public creatorTokens ;
+    mapping(address => uint256)public tokenIndex ;
 }
